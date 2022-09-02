336cdf04b903de3ac35d882abfa7deda023dc67e
Raquibul Islam <ranacse05@gmail.com>
2022-09-02 20:04:26 +0600

    Index added

--- index.php
+++ index.php
@@ -4,3 +4,17 @@
  *
  * @package Acamar
  */
+?>
+<!doctype html> 
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Wordpress - Just another WordPress site</title>
+</head>
+<body>
+    <h1>Hello !! Acamer</h1>
+</body>
+</html>
